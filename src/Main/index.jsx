476f4f30b51ed5661f8082b9a2432a235c80950b
Aviad Elya <0527563488a@gmail.com>
2023-02-09 12:43:00 +0200

    afetr logice

--- src/Main/index.jsx
+++ src/Main/index.jsx
@@ -4,15 +4,24 @@ import React, { useState } from 'react'
 import Calculator from '../function/Calculator'
 import Button from '../components/Button'
 import { FiDelete } from 'react-icons/fi'
+import { RiDeleteBin2Line } from 'react-icons/ri'
 import style from './style.module.css'
 
 function Main() {
-  const [arrangArray, setarrangArray] = useState([])
+  const [arrangArray, setArrangArray] = useState("")
   const [solution, setSolution] = useState('')
+
+  //to add anumber or func to string arrangArray
   const sever = (e) => {
-    if (e.target.value == "=") { setSolution(Calculator(arrangArray)) }
-    setarrangArray([...arrangArray,e.target.value])
+    setArrangArray(arrangArray+e.target.value)
   }
+  //=to process the data and return answer
+  const equal = (e) => {
+    console.log(arrangArray);
+    setSolution(`=${Calculator(arrangArray)}`)
+  }
+  //function for delet all button
+  function deletAll(){setArrangArray("");setSolution("")}
 
 
   return (
@@ -29,19 +38,19 @@ function Main() {
             <td>  <Button name="number" value={9} onclick={sever} data="9" /></td>
             <td>   <Button name="number" value={8} onclick={sever} data="8" /></td>
             <td>  <Button name="number" value={7} onclick={sever} data="7" /></td>
-            <td><Button name="func" value={"+"} onclick={sever} data={<FiDelete />} /></td>
+            <td><Button name="func" onclick={()=>{setArrangArray(arrangArray.slice(0,-1))}} data={<FiDelete />} /></td>
           </tr>
           <tr>
             <td>    <Button name="number" value={6} onclick={sever} data="6" /></td>
             <td>   <Button name="number" value={5} onclick={sever} data="5" /></td>
             <td>   <Button name="number" value={4} onclick={sever} data="4" /></td>
-            <td>  <Button name="func" value={"="} onclick={sever} data="=" /></td>
+            <td>  <Button name="=" value={"="} onclick={equal} data="=" /></td>
           </tr>
           <tr>
             <td>    <Button name="number" value={3} onclick={sever} data="3" /></td>
             <td>   <Button name="number" value={2} onclick={sever} data="2" /></td>
             <td>   <Button name="number" value={1} onclick={sever} data="1" /></td>
-            <td>  <Button name="func" value={"#"} onclick={sever} data="#" /></td>
+            <td>  <Button name="funcDeletAll" onclick={deletAll} data={<RiDeleteBin2Line />} /></td>
           </tr>
           <tr>
             <td>    <Button name="number" value={0} onclick={sever} data="0" /></td>
@@ -49,7 +58,7 @@ function Main() {
           </tr>
         </tbody>
       </table>
-      {arrangArray.map(v=>v)}{solution}
+      {arrangArray}{solution}
     </div>
   )
 }
--- src/function/Calculator/index.js
+++ src/function/Calculator/index.js
@@ -1,42 +1,46 @@
 //calculator function
 
 export default function Calculator(array1) {
-    let arrayWork = array1.map(value => {
-        return isNaN(value) ? value : parseInt(value, 10);})
+let array2 = [...array1.matchAll(/[+\-*/]|[^+\-*/]+/g)];
+console.log(array2);
+array2 = array2.map(x => x[0]);
+    console.log(array2);
+    
+    let arrayWork = array2.map(value => {
+        return isNaN(value) ? value : parseInt(value, 10);
+    })
     console.log(arrayWork);
-        for (let ind = 0; ind < arrayWork.length - 1; ind++) {
-            if (arrayWork[ind] === "/") {
-                let temp = (arrayWork[ind-1])/(arrayWork[ind+1]);
-                arrayWork[ind-1] = temp;
-                arrayWork.splice(ind, 2)
-            }
+    for (let ind = 0; ind < arrayWork.length - 1; ind++) {
+        if (arrayWork[ind] === "/") {
+            let temp = (arrayWork[ind - 1]) / (arrayWork[ind + 1]);
+            arrayWork[ind - 1] = temp;
+            arrayWork.splice(ind, 2)
         }
-        console.log(1);
-        for (let ind = 0; ind < arrayWork.length - 1; ind++) {
-            if (arrayWork[ind] === "*") {
-                let temp = (arrayWork[ind-1])*(arrayWork[ind+1]);
-               arrayWork[ind-1] = temp;
-                console.log(arrayWork[ind-1]);
-                arrayWork.splice(ind, 2)
-                console.log(arrayWork);
-            }
+    }
+    
+    for (let ind = 0; ind < arrayWork.length - 1; ind++) {
+        if (arrayWork[ind] === "*") {
+            let temp = (arrayWork[ind - 1]) * (arrayWork[ind + 1]);
+            arrayWork[ind - 1] = temp;
+            arrayWork.splice(ind, 2)
         }
-        console.log(2);
-        for (let ind = 0; ind < arrayWork.length - 1; ind++) {
-            if (arrayWork[ind] === "+") {
-                let temp = (arrayWork[ind-1])+(arrayWork[ind+1]);
-               arrayWork[ind-1] = temp;
-                arrayWork.splice(ind, 2)
-            }
+    }
+    
+    for (let ind = 0; ind < arrayWork.length - 1; ind++) {
+        if (arrayWork[ind] === "+") {
+            let temp = (arrayWork[ind - 1]) + (arrayWork[ind + 1]);
+            arrayWork[ind - 1] = temp;
+            arrayWork.splice(ind, 2)
         }
-        console.log(3);
-        for (let ind = 0; ind < arrayWork.length - 1; ind++) {
-            if (arrayWork[ind] === "-") {
-                let temp = (arrayWork[ind-1])-(arrayWork[ind+1]);
-                arrayWork[ind-1] = temp;
-                arrayWork.splice(ind, 2)
-            }
+    }
+    
+    for (let ind = 0; ind < arrayWork.length - 1; ind++) {
+        if (arrayWork[ind] === "-") {
+            let temp = (arrayWork[ind - 1]) - (arrayWork[ind + 1]);
+            arrayWork[ind - 1] = temp;
+            arrayWork.splice(ind, 2)
         }
+    }
     return arrayWork
 }
 
